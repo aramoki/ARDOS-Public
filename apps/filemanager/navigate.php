ed7a56e7d950eb40cbec2e95159fd8f052c1e46c
aramok <mohorame@gmail.com>
2016-11-11 21:59:19 +0200

    v1.2.7.2 minor changes

--- apps/filemanager/navigate.php
+++ apps/filemanager/navigate.php
@@ -9,11 +9,20 @@
 include '../../kernel.php';
 $directory = filter_input(INPUT_POST, "directory", FILTER_DEFAULT);
 $window_id = filter_input(INPUT_POST, "window_id", FILTER_DEFAULT);
+$username = filter_input(INPUT_COOKIE, 'username', FILTER_DEFAULT);
 
 
 $filereader = new FIO();
 $UI = new UI_manager($filereader);
 $icons = $filereader->list_dir($directory);
-//print_r($icons);
 
-echo $UI->draw_icons($icons,$theme, 0 , $window_id);
+//depletion need here
+$level = count(split('/',str_replace(ABSPATH,'',$directory.'/')));
+
+if (strpos($directory.'/', ABSPATH) !== false || isset($username)) {
+    echo $UI->draw_icons($icons,$theme, 0 , $window_id);
+}else{
+    echo 'Forbidden';
+}
+
+
